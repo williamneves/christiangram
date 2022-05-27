97bb37430119f8f93af5e7fb6ccf7469268aac8e
William Neves <drwilliamneves@gmail.com>
2022-05-27 00:16:55 -0400

    remove fw

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,6 @@ import '../styles/globals.css';
 // import 'tailwindcss/tailwind.css'
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
 
 function MyApp( { Component, pageProps: { session, ...pageProps } } ) {
 	
